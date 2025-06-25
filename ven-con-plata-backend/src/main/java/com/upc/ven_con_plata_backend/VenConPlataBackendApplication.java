4dcea54681e791cebd9cf09dc800b172ac1a658e
BGamio <u202120344@upc.edu.pe>
2025-06-25 00:11:55 -0500

    fix: added pom.xml as Maven in the project

--- ven-con-plata-backend/src/main/java/com/upc/ven_con_plata_backend/VenConPlataBackendApplication.java
+++ ven-con-plata-backend/src/main/java/com/upc/ven_con_plata_backend/VenConPlataBackendApplication.java
@@ -2,12 +2,16 @@ package com.upc.ven_con_plata_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VenConPlataBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VenConPlataBackendApplication.class, args);
+
 	}
 
 }
