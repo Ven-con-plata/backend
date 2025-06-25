13181419b388de635a2a42dee70716b2a5ed1ff0
BGamio <u202120344@upc.edu.pe>
2025-06-24 19:04:07 -0500

    chore: initialized java spring boot proyect

---
+++ ven-con-plata-backend/src/main/java/com/upc/ven_con_plata_backend/VenConPlataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.upc.ven_con_plata_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenConPlataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenConPlataBackendApplication.class, args);
+	}
+
+}
